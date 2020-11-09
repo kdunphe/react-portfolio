851e4ba3ed93a699945b70dd19e0f5d61d9aa9c3
kdunphe <57336712+kdunphe@users.noreply.github.com>
2020-11-08 22:30:09 -0500

    Updates

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $(".menu-icon").on("click", function () {
+        $('nav ul').toggleClass('showing');
+    });
+});
+
+$('.menu ul li a').on("click", function() {
+    $('.menu-icon').click();
+});
+
+
+// Button Hover //
+
+$("button").hover(function () {
+    $(this).css("background-color", "lightgray");
+}, function () {
+    $(this).css("background-color", "gray");
+});
