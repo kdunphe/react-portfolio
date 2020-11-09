8ab901b34c9c85e80dffb1226b6b4db507dd9e13
kdunphe <57336712+kdunphe@users.noreply.github.com>
2020-11-08 21:58:33 -0500

    Updating contact component

---
+++ src/components/Contact/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./style.css";
+
+function Contact() {
+    return (
+        <main id="contact">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-1"></div>
+                    <div class="col-sm-10">
+                        <h1>Contact</h1>
+                        <br />
+                        <form>
+                            <div class="form-group">
+                                <label for="name">Name</label>
+                                <input type="text" placeholder="Name" class="form-control" id="name" aria-describedby="name" />
+                            </div>
+                            <div class="form-group">
+                                <label for="email">Email</label>
+                                <input type="email" placeholder="Email" class="form-control" id="email"
+                                    aria-describedby="email" />
+                            </div>
+                            <div class="form-group">
+                                <label for="message">Message</label>
+                                <textarea class="form-control" placeholder="Message" id="message" rows="3" aria-describedby="message"></textarea>
+                            </div>
+                            <br />
+                            <button type="submit" class="btn btn-info">Submit</button>
+                        </form>
+                    </div>
+                    <div class="col-sm-1"></div>
+                </div>
+            </div>
+        </main>
+    );
+}
+
+export default Contact;
